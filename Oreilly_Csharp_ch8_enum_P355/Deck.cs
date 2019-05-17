0d50b94e6ae6a563ceedb9d8333eff1632848db5
Peng <Peng@PENG-PC>
2019-05-17 14:40:31 +0800

    P357 temp

---
+++ Oreilly_Csharp_ch8_enum_P355/Deck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch8_enum_P355
+{
+    class Deck
+    {
+        private Card[] cards =
+        {
+            new Card (Suits.Spades, Values.Ace)  
+            //後面還有51張牌
+
+
+
+
+
+        };         
+    }
+}
