9834c2aa08b07080ba0a704d31f6d766cc2bdb23
Peng <Peng@PENG-PC>
2019-05-17 09:55:24 +0800

    加入專案檔案。

---
+++ Oreilly_Csharp_ch8_enum_P355/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oreilly_Csharp_ch8_enum_P355
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
